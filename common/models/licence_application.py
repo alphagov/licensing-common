16da4376f21b0c300ef339daf01e983e33253d3c
amy-a-oconnor <amy.o'connor@madetech.com>
2026-07-27 15:29:58 +0100

    Add next Embedded Model

--- common/models/licence_application.py
+++ common/models/licence_application.py
@@ -6,9 +6,14 @@ from common.models.licences import SupportingDocumentDefinition
 
 
 class SupportingDocument(EmbeddedModel):
-    filename = models.CharField(blank=True, max_length=255, db_column="filename")
+    filename = models.CharField(db_column="filename", max_length=255, blank=True)
     definition = EmbeddedModelField(SupportingDocumentDefinition, db_column="definition")
     _id = ObjectIdField(db_column="_id")
-    virus_check_status = models.CharField(max_length=255, db_column="virusCheckStatus")#"Clean" or "FoundVirus"
+    virus_check_status = models.CharField(db_column="virusCheckStatus", max_length=255)#"Clean" or "FoundVirus"
     # stream = runtime only field
+
+class SupportingDocumentDefinition(EmbeddedModel):
+    name = models.CharField(db_column="name", max_length=255)
+    description = models.CharField(db_column="description", max_length=255, default="", blank=True)
+    required = models.BooleanField(db_column="isMandatory", default=False)
 
