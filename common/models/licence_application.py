9bc8fc0b33dd7875b4cfae165bebfa3d43d7be15
amy-a-oconnor <amy.o'connor@madetech.com>
2026-07-27 15:49:09 +0100

    Add next Embedded Model

--- common/models/licence_application.py
+++ common/models/licence_application.py
@@ -1,3 +1,4 @@
+import bson
 from django.db import models
 from django_mongodb_backend.fields import EmbeddedModelField, ObjectIdField
 from django_mongodb_backend.models import EmbeddedModel
@@ -10,10 +11,16 @@ class SupportingDocument(EmbeddedModel):
     definition = EmbeddedModelField(SupportingDocumentDefinition, db_column="definition")
     _id = ObjectIdField(db_column="_id")
     virus_check_status = models.CharField(db_column="virusCheckStatus", max_length=255)#"Clean" or "FoundVirus"
-    # stream = runtime only field
+# stream = runtime only field
 
 class SupportingDocumentDefinition(EmbeddedModel):
     name = models.CharField(db_column="name", max_length=255)
     description = models.CharField(db_column="description", max_length=255, default="", blank=True)
     required = models.BooleanField(db_column="isMandatory", default=False)
+
+class Service(EmbeddedModel):
+    licence_code = models.CharField(db_column="licenseCode", max_length=255)
+    interaction_id = models.IntegerField(db_column="lgilId")
+    interaction_sub_id = models.IntegerField(db_column="lgilSubId")
+# consider renaming this model to something more relevant or 'purging' as noted in scala code
 
