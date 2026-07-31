4bbd3d9a7963c8c4f99366b5bbcd93e1e22408e0
amy-a-oconnor <amy.o'connor@madetech.com>
2026-07-31 18:47:27 +0100

    refactor test to utilise message_dict

--- common/tests/model/test_licence_application.py
+++ common/tests/model/test_licence_application.py
@@ -18,7 +18,7 @@ def test_valid_licence_application():
         licence="test",
         supporting_documents_online=True,
         application_document=SupportingDocument(
-            definition=SupportingDocumentDefinition(), _id="id", virus_check_status=VirusCheckStatus.CLEAN.value
+            definition=SupportingDocumentDefinition(), _id=ObjectId(), virus_check_status=VirusCheckStatus.CLEAN.value
         ),
         licence_code="test",
         interaction_id=InteractionIdCodes.APPLY.value,
@@ -43,37 +43,10 @@ def test_valid_licence_application():
 def test_invalid_interaction_id_in_licence_application_service_throws_error():
     expected_error_message = "Value 1 is not a valid choice."
     with pytest.raises(ValidationError) as e:
-        licence_application = LicenceApplication(
-            applicant_email="test@test.com",
-            authority="test",
-            licence="test",
-            supporting_documents_online=True,
-            application_document=SupportingDocument(
-                filename="test",
-                definition=SupportingDocumentDefinition(),
-                _id=ObjectId(),
-                virus_check_status=VirusCheckStatus.CLEAN.value,
-            ),
-            licence_code="test",
-            interaction_id=1,
-            interaction_sub_id=0,
-            status=ApplicationStatus(data_available=False, is_being_processed=False, collected_by_authority=False),
-            application_date=now(),
-            application_reference="test",
-            authority_application_reference="test",
-            expected_processing_date=now() + timedelta(days=7),
-            tacit_consent=False,
-            required_payment_amount=PaymentAmount(),
-            fee_required=False,
-            variable_fee=False,
-            payment_reference_id="test",
-            application_main_form="test",
-            collected_by="test@test.com",
-            under_process_by="test@test.com",
-        )
+        licence_application = LicenceApplication(interaction_id=1)
         licence_application.full_clean()
 
-    assert e.value.messages == [expected_error_message]
+    assert expected_error_message in e.value.message_dict["interaction_id"]
 
 
 def test_is_virus_detected_in_file():
