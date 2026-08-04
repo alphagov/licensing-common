9b2aa81b36eaf83167ce91bcc649207dcf451f9c
amy-a-oconnor <amy.o'connor@madetech.com>
2026-08-04 13:38:46 +0100

    remove unused validate_country_codes

--- common/models/utils.py
+++ common/models/utils.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
-from common.enums.countries import Countries, CountryCodes
+from common.enums.countries import Countries
 from common.enums.snac_codes import SnacCodes
 
 
@@ -8,11 +8,6 @@ def validate_countries(countries: list):
     for country in countries:
         if country not in Countries:
             raise ValidationError("Invalid country")
-
-
-def validate_country_code(country_code: str):
-    if country_code not in CountryCodes:
-        raise ValidationError("Invalid country code")
 
 
 def validate_snac_codes(snac_codes: list):
