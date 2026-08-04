6e68a225fd6c84a63bd585d6e09c00e4fdfab329
amy-a-oconnor <amy.o'connor@madetech.com>
2026-08-04 15:19:26 +0100

    remove unused utils file

--- common/models/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-from common.enums.snac_codes import SnacCodes
-
-
-def validate_snac_codes(snac_codes: list):
-    if not set(snac_codes).issubset(set(SnacCodes.list())):
-        raise ValidationError("Snac codes not valid")
