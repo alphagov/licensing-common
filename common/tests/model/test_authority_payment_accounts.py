adbef3fc4551a29de126b23e454c9877bdb741e0
amy-a-oconnor <amy.o'connor@madetech.com>
2026-08-06 15:22:33 +0100

    Add passing test for valid non-civica payment provider

--- common/tests/model/test_authority_payment_accounts.py
+++ common/tests/model/test_authority_payment_accounts.py
@@ -10,7 +10,7 @@ from common.models.authority_payment_accounts import (
 )
 
 
-def test_valid_authority_payment_accounts():
+def test_valid_authority_payment_accounts_with_civica():
     authority_payment_accounts = AuthorityPaymentAccounts(
         authority_url_slug="test",
         payment_provider=PaymentProviders.CIVICA,
@@ -27,14 +27,40 @@ def test_valid_authority_payment_accounts():
         merchant_id="test",
         provider_shared_secret="test",
         provider_shared_post_salt="test",
-        civica_payment_url="test",
+        civica_payment_form_url="test",
         civica_payment_verify_url="test",
-        civica_payment_username="test",
-        civica_payment_password="test",
-        civica_payment_security_domain="test",
-        civica_username_2="test",
-        civica_password_2="test",
-        civica_icon_security_use=False,
+        use_icon_security=False,
+        iron_security_auth_username="test",
+        iron_security_auth_password="test",
+        payment_security_domain="test",
+        basic_auth_username="test",
+        basic_auth_password="test",
+        mac_secret_key="test",
+        callback_override_url="test",
+        send_test_payment=False,
+        access_pay_suite_accepted_cards=AccessPaySuiteAcceptedCards(),
+        worldpay_md5_shared_secret="test",
+    )
+    authority_payment_accounts.full_clean()
+
+
+def test_valid_authority_payment_accounts_without_civica():
+    authority_payment_accounts = AuthorityPaymentAccounts(
+        authority_url_slug="test",
+        payment_provider=PaymentProviders.NORTHGATE,
+        accounts=[
+            PaymentAccount(
+                name="test",
+                provider_specific_code_A="test",
+                provider_specific_code_B="test",
+                provider_specific_code_C="test",
+                provider_specific_code_D="test",
+                account_id=ObjectId(),
+            )
+        ],
+        merchant_id="test",
+        provider_shared_secret="test",
+        provider_shared_post_salt="test",
         mac_secret_key="test",
         callback_override_url="test",
         send_test_payment=False,
