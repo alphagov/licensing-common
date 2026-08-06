fb2cb3ca2e0ae9936b067e2def63ac59780c2bce
amy-a-oconnor <amy.o'connor@madetech.com>
2026-08-06 11:56:37 +0100

    Pass test for valid authority payment accounts

--- common/models/authority_payment_accounts.py
+++ common/models/authority_payment_accounts.py
@@ -7,10 +7,10 @@ from common.enums.payment_providers import PaymentProviders
 
 class PaymentAccount(EmbeddedModel):
     name = models.CharField(db_column="name", max_length=255)
-    provider_specific_codeA = models.CharField(db_column="codeA", max_length=255)
-    provider_specific_codeB = models.CharField(db_column="codeB", max_length=255)
-    provider_specific_codeC = models.CharField(db_column="codeC", max_length=255)
-    provider_specific_codeD = models.CharField(db_column="codeD", max_length=255)
+    provider_specific_code_A = models.CharField(db_column="codeA", max_length=255)
+    provider_specific_code_B = models.CharField(db_column="codeB", max_length=255)
+    provider_specific_code_C = models.CharField(db_column="codeC", max_length=255)
+    provider_specific_code_D = models.CharField(db_column="codeD", max_length=255)
     account_id = ObjectIdField(db_column="acc_id", unique=True, blank=True, null=True)
 
 
--- common/tests/model/test_authority_payment_accounts.py
+++ common/tests/model/test_authority_payment_accounts.py
@@ -1,2 +1,42 @@
+from bson import ObjectId
+
+from common.enums.payment_providers import PaymentProviders
+from common.models.authority_payment_accounts import (
+    AccessPaySuiteAcceptedCards,
+    AuthorityPaymentAccounts,
+    PaymentAccount,
+)
+
+
 def test_valid_authority_payment_accounts():
-    pass
+    authority_payment_accounts = AuthorityPaymentAccounts(
+        authority_url_slug="test",
+        payment_provider=PaymentProviders.CIVICA,
+        accounts=[
+            PaymentAccount(
+                name="test",
+                provider_specific_code_A="test",
+                provider_specific_code_B="test",
+                provider_specific_code_C="test",
+                provider_specific_code_D="test",
+                account_id=ObjectId(),
+            )
+        ],
+        merchant_id="test",
+        provider_shared_secret="test",
+        provider_shared_post_salt="test",
+        civica_payment_url="test",
+        civica_payment_verify_url="test",
+        civica_payment_username="test",
+        civica_payment_password="test",
+        civica_payment_security_domain="test",
+        civica_username_2="test",
+        civica_password_2="test",
+        civica_icon_security_use=False,
+        mac_secret_key="test",
+        callback_override_url="test",
+        send_test_payment=False,
+        access_pay_suite_accepted_cards=AccessPaySuiteAcceptedCards(),
+        worldpay_md5_shared_secret="test",
+    )
+    authority_payment_accounts.full_clean()
