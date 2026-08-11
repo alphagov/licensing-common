e240608f8d558d788c447a87329c2c33a6b4359c
simonfreeman-ux <simon.freeman@digital.cabinet-office.gov.uk>
2026-08-11 11:57:02 +0100

    test: payment hydration

---
+++ common/tests/hydration/test_payments_hydration.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+from common.models.payments import Payment
+from common.tests.utils.hydration import verify_model_against_collection
+from config import settings
+
+
+def test_django_models_match_document(show_diffs):
+    with MongoClient(settings.DOCUMENT_DB_CONN) as client:
+        db = client["licensify"]
+        errors = verify_model_against_collection(db, Payment, "all", show_diffs)
+        assert errors == []
