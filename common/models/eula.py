01e14acc2e37dbabc9c309751e19c4795161bf4f
Stephane torres <stephane.torres@madetech.com>
2026-08-12 14:18:11 +0100

    add eula model

---
+++ common/models/eula.py
@@ -0,0 +1,25 @@
+import bson
+from django.db import models
+from django.utils import timezone
+from django_mongodb_backend.fields import EmbeddedModelArrayField, ObjectIdField
+from django_mongodb_backend.models import EmbeddedModel
+
+
+class EulaAcceptances(EmbeddedModel):
+    email = models.EmailField()
+    accepted_on = models.DateTimeField(db_column="acceptedOn")
+
+
+class Eula(models.Model):
+    _id = ObjectIdField(db_column="_id", auto_created=True, default=bson.ObjectId, primary_key=True)
+    version = models.CharField(max_length=255, editable=False, unique=True)
+    valid_from = models.DateTimeField(db_column="validFrom", default=timezone.now)
+    html_text = models.TextField(db_column="htmlText")
+    acceptances = EmbeddedModelArrayField(EulaAcceptances, blank=True, null=True, default=list)
+
+    class Meta:
+        db_table = "eula"
+        managed = False
+
+    def __str__(self):
+        return f"{self.version}"
---
+++ common/tests/model/test_eula.py
@@ -0,0 +1,40 @@
+import pytest
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+from common.models.eula import Eula
+
+
+def test_valid_eula():
+    eula = Eula(
+        version="1.0",
+        valid_from=timezone.now(),
+        html_text="<p>This is an eula version</p>",
+    )
+
+    eula.full_clean()
+
+
+def test_creating_new_eula_with_same_version_throws_error(db_tracker, db_cleanup):
+    expected_error_message = "Eula with this Version already exists."
+    pre_existing_eula = Eula.objects.create(
+        version="2.0",
+        valid_from=timezone.now(),
+        html_text="<p>This is an eula version</p>",
+    )
+
+    db_tracker.register_created(pre_existing_eula._id)
+
+    with pytest.raises(ValidationError) as e:
+        new_eula = Eula(
+            version="2.0",
+            valid_from=timezone.now(),
+            html_text="<p>This is an eula version, this should fail to create</p>",
+        )
+
+        new_eula.full_clean()
+
+    assert expected_error_message in e.value.messages
+    assert len(e.value.messages) == 1
+
+    db_cleanup(Eula, db_tracker.created_ids)
