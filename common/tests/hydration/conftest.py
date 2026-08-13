afc585900f83210b4f0420cf7a09e64e8e628ac6
simonfreeman-ux <simon.freeman@digital.cabinet-office.gov.uk>
2026-08-13 14:41:50 +0100

    remove unecessary db unblocker fixture

--- common/tests/hydration/conftest.py
+++ common/tests/hydration/conftest.py
@@ -12,18 +12,6 @@ def check_db_online():
         pytest.skip("Local DocumentDB connection failed. Skipping test.")
 
 
-@pytest.fixture(autouse=True)
-def unblock_db_for_hydration_tests(django_db_blocker):
-    """
-    By default, pytest blocks calls to a db because it wants to make a test db.
-    The hydration tests are designed to use a local database not spin up a test db.
-    This function lifts the lock on database queries.
-    @pytest.mark.django_db can't be used because that will sping up a test db.
-    """
-    with django_db_blocker.unblock():
-        yield
-
-
 def pytest_addoption(parser):
     parser.addoption(
         "--show-diffs",
