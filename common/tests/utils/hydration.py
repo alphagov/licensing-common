721881983b6ecb10a1f6e5fe7b8b539d10db5947
simonfreeman-ux <simon.freeman@digital.cabinet-office.gov.uk>
2026-08-17 17:07:41 +0100

    tweaks: names and comments
    removing uneeded comments, slight refactors on name changes, using inline fors for data, etc

--- common/tests/utils/hydration.py
+++ common/tests/utils/hydration.py
@@ -11,7 +11,7 @@ from pymongo.database import Database
 logger = logging.getLogger(__name__)
 
 
-def verify_model_against_collection(db: Database, model_class: type[models.Model], sample_size=None, show_diffs=False):
+def verify_model_against_collection(db: Database, model_class: type[models.Model], sample_size=0, show_diffs=False):
     """
     This function compares actual documents from the database (via pymongo)  with a django model and reports any issues.
 
@@ -23,27 +23,30 @@ def verify_model_against_collection(db: Database, model_class: type[models.Model
     increase confidence in initial project release but can help detect database drift and bad data.
     """
     collection_name = model_class._meta.db_table
-    # sample checks
-    if sample_size == "all" or sample_size is None:
+
+    if sample_size == 0:
         sample_docs = list(db[collection_name].find())
     else:
         sample_docs = list(db[collection_name].aggregate([{"$sample": {"size": sample_size}}]))
+
     actual_sample_size = len(sample_docs)
     if actual_sample_size == 0:
         raise ValueError(f"Collection '{collection_name}' has no data")
-    elif isinstance(sample_size, int) and actual_sample_size < sample_size:
-        warnings.warn(
-            f"{sample_size} asked for, only {actual_sample_size} found.",
-            category=UserWarning,
-            stacklevel=2,
-        )
-    elif actual_sample_size < 100:
-        warnings.warn(
-            "Less than 100 items sampled",
-            category=UserWarning,
-            stacklevel=2,
-        )
-    mismatches = _check_for_mismatches(model_class, sample_docs, show_diffs)
+    else:
+        if isinstance(sample_size, int) and actual_sample_size < sample_size:
+            warnings.warn(
+                f"{sample_size} asked for, only {actual_sample_size} found.",
+                category=UserWarning,
+                stacklevel=2,
+            )
+        if actual_sample_size < 100:
+            warnings.warn(
+                "Less than 100 items sampled, this may not be a fully representative data set.",
+                category=UserWarning,
+                stacklevel=2,
+            )
+
+    mismatches = _get_mismatches(model_class, sample_docs, show_diffs)
     logger.info(
         "[%s] Verified %s docs in '%s'. Found %s mismatch(es).",
         model_class.__name__,
@@ -54,17 +57,13 @@ def verify_model_against_collection(db: Database, model_class: type[models.Model
     return mismatches
 
 
-def _check_for_mismatches(model_class: type[models.Model], sample_docs, show_diffs=False):
-    # make sure it returns at least an empty array for mismatches
+def _get_mismatches(model_class: type[models.Model], sample_docs, show_diffs=False):
     mismatches = []
-    # create a dictionary that maps database attribute names to django field names
-    db_attribute_to_field = _get_explicitly_declared_attributes(model_class)
 
-    # actual comparisons start here
+    db_attribute_to_django_field_dictionary = _get_explicitly_declared_attributes(model_class)
+    real_primary_key_col = model_class._meta.pk.column
+
     for doc in sample_docs:
-        real_primary_key_col = model_class._meta.pk.column
-
-        # check data exists in both
         doc_id = doc[real_primary_key_col]
         try:
             django_obj = model_class.objects.get(pk=doc_id)
@@ -72,30 +71,31 @@ def _check_for_mismatches(model_class: type[models.Model], sample_docs, show_dif
             mismatches.append(f"[{model_class.__name__}] Doc _id={doc_id} missing in Django ORM.")
             continue
 
-        # forces the validation rules to run, so enum properties and custom rules that normally only occur on form fills
-        # tests the model matches reality (or the quality of the data)
-        validation_errors = _get_clean_errors(django_obj, model_class)
-        for err in validation_errors:
-            mismatches.append(f"[{model_class.__name__}] Doc _id={doc_id}: {err}")
+        mismatches.extend(f"[{model_class.__name__}] Doc _id={doc_id}: {err}" for err in _get_clean_errors(django_obj))
 
-        # check data structure and values match
-        for raw_key, raw_val in doc.items():
-            # if there's an _id BUT it isn't the primary key of the model AND we haven't mapped it, let's assume it's
-            # an intentional choice to leave it out
-            if raw_key == "_id" and real_primary_key_col != "_id" and "_id" not in db_attribute_to_field:
+        for raw_attribute_key, raw_val in doc.items():
+            # if there's an _id BUT we chose to use a different primary key, let's assume that exclusion is intended
+            # and ignore the fact it's in the database and not on the model
+            if (
+                raw_attribute_key == "_id"
+                and real_primary_key_col != "_id"
+                and "_id" not in db_attribute_to_django_field_dictionary
+            ):
                 continue
-            # log missing properties at the top level
-            structure_mismatch = _check_data_structure(model_class, db_attribute_to_field, raw_key)
-            if structure_mismatch is not None:
+
+            structure_mismatch = _get_data_structure_errors(
+                model_class, db_attribute_to_django_field_dictionary, raw_attribute_key
+            )
+            if structure_mismatch:
                 mismatches.append(structure_mismatch)
                 continue
-            # if the structure is fine, check values
-            field_name = db_attribute_to_field[raw_key]
-            mismatches += _check_data_values(model_class, field_name, raw_val, doc_id, django_obj, show_diffs)
+
+            field_name = db_attribute_to_django_field_dictionary[raw_attribute_key]
+            mismatches.extend(_get_data_value_errors(model_class, field_name, raw_val, doc_id, django_obj, show_diffs))
     return mismatches
 
 
-def _get_clean_errors(django_obj, model_class):
+def _get_clean_errors(django_obj):
     clean_errors = []
 
     try:
@@ -115,48 +115,46 @@ def _is_django_implicit_auto_pk(field: models.Field) -> bool:
 
 
 def _get_explicitly_declared_attributes(model_class):
-    # basically we're trying to avoid useless django ids
+    """
+    This is a workaround designed to ignore any auto-injected django attributes, most notably false primary keys
+    """
     return {field.column: field.name for field in model_class._meta.fields if not _is_django_implicit_auto_pk(field)}
 
 
-def _check_data_structure(model_class, db_attribute_to_field, raw_key):
-    if raw_key not in db_attribute_to_field:
-        return f"[{model_class.__name__}] Key '{raw_key}' in DB missing from Django model."
+def _get_data_structure_errors(model_class, db_attribute_to_field, document_attribute):
+    """
+    Returns an error message if there is no django property mapped to a document
+    """
+    if document_attribute not in db_attribute_to_field:
+        return f"[{model_class.__name__}] Attribute '{document_attribute}' in DB missing from Django model."
     return None
 
 
-def _check_data_values(model_class, field_name, raw_val, doc_id, django_obj, show_diffs=False):
+def _get_data_value_errors(model_class, field_name, raw_val, doc_id, django_obj, show_diffs=False):
     value_mismatches = []
     django_val = getattr(django_obj, field_name)
 
-    # to compare we need common ground, normalising the data gets both in a comparable format such as ints, or
-    # a dictionary, etc,  and handles known data type quirks
-    normalised_pymongo_result = normalise_pymongo_result(raw_val)
-    normalised_django = _normalise_django(django_val)
+    # the normalise functions remove the data noise and standardise both data to dictionaries
+    pymongo_result_dictionary = normalise_pymongo_result(raw_val)
+    django_model_data_dictionary = _normalise_django(django_val)
 
-    _strip_django_defaults(normalised_pymongo_result, normalised_django)
+    _strip_django_defaults(pymongo_result_dictionary, django_model_data_dictionary)
     diffs = DeepDiff(
-        normalised_pymongo_result,
-        normalised_django,
+        pymongo_result_dictionary,
+        django_model_data_dictionary,
         ignore_numeric_type_changes=True,
         ignore_order=True,
     )
-    # at this point we should have 2 identical values, whether it's basic ints or a complex nested dictionary
-    # if we don't then the model does not represent the database.
-    if diffs:
-        for category, items in diffs.items():
-            for item in items:
-                # instead of root, use the actual field name
-                useable_item = str(item).replace("root", field_name, 1)
-                value_mismatches.append(
-                    f"[{model_class.__name__}] model error. Doc _id: {doc_id}\n"
-                    f"  Db value does not match model value. {category} at {useable_item}"
-                )
 
-        # use pytest common/tests/hydration/[test_here].py -s --show-diffs for this to log
-        # this is to make it almost impossible for any PII to get logged accidentally
+    if diffs:
+        value_mismatches = [
+            f"[{model_class.__name__}] model error. Doc _id: {doc_id}\n"
+            f"  Db value does not match model value. {category} at {str(item).replace('root', field_name, 1)}"
+            for category, items in diffs.items()
+            for item in items
+        ]
+        # this flag is to make it almost impossible for any PII to get logged accidentally
         if show_diffs:
-            logger.warning(doc_id)
             pretty = diffs.pretty()
             useable_pretty = str(pretty).replace("root", field_name, 1)
             logger.warning("Issue for document %s: \n %s", doc_id, useable_pretty)
@@ -164,7 +162,7 @@ def _check_data_values(model_class, field_name, raw_val, doc_id, django_obj, sho
     return value_mismatches
 
 
-def _normalise_common(val, normaliser_fn):
+def _get_normalised_dictionary(val, normaliser_fn):
     """
     Handles common cleaning tasks like forcing embedded documents/models to keep recursing through until all the
     primitive data attributes have been normalised and continues
@@ -195,7 +193,7 @@ def normalise_pymongo_result(val):
     if isinstance(val, Decimal128):
         return val.to_decimal()
 
-    return _normalise_common(val, normalise_pymongo_result)
+    return _get_normalised_dictionary(val, normalise_pymongo_result)
 
 
 def _normalise_django(val):
@@ -207,21 +205,18 @@ def _normalise_django(val):
     if val is None:
         return None
 
-    # if it's embedded we skip primary keys because they wont have them in the db
+    # for embedded models, skip over autogenerated django primary keys
     if isinstance(val, EmbeddedModel):
-        normalised_dict = {}
-        for field in val._meta.fields:
-            if _is_django_implicit_auto_pk(field):
-                continue
-            field_value = getattr(val, field.name)
-            normalised_dict[field.column] = _normalise_django(field_value)
-        return normalised_dict
+        return {
+            field.column: _normalise_django(getattr(val, field.name))
+            for field in val._meta.fields
+            if not _is_django_implicit_auto_pk(field)
+        }
 
-    # regular top level primary key
-    if hasattr(val, "_meta"):
+    if hasattr(val, "_meta"):  # if not embedded model
         return _normalise_django(val.pk)
 
-    return _normalise_common(val, _normalise_django)
+    return _get_normalised_dictionary(val, _normalise_django)
 
 
 def _strip_django_defaults(normalised_pymongo_result_val, django_val):
@@ -230,14 +225,12 @@ def _strip_django_defaults(normalised_pymongo_result_val, django_val):
     In the database, even if a field is omitted, the django model will still give it a default value of "" or none.
     """
     if isinstance(normalised_pymongo_result_val, dict) and isinstance(django_val, dict):
-        # Delete unmapped legacy primary key noise from pymongo result ('id', '_id') if not present in normalized Django
-        # output
+        # Delete unmapped legacy primary key noise from pymongo result ('id', '_id') if not present on django model
         for pk_key in ("id", "_id"):
             if pk_key in normalised_pymongo_result_val and pk_key not in django_val:
                 del normalised_pymongo_result_val[pk_key]
 
         for key in list(django_val.keys()):
-            # this does assume the default value
             if key not in normalised_pymongo_result_val and django_val[key] in (None, "", [], {}):
                 del django_val[key]
             # recurse in case it's an embedded model that needs fixing
--- common/tests/utils/tests/test_hydration.py
+++ common/tests/utils/tests/test_hydration.py
@@ -2,8 +2,8 @@ import pytest
 from bson import ObjectId
 
 from common.tests.utils.hydration import (
-    _check_data_structure,
-    _check_data_values,
+    _get_data_structure_errors,
+    _get_data_value_errors,
     _get_explicitly_declared_attributes,
     _strip_django_defaults,
 )
@@ -54,22 +54,20 @@ def mock_department_ultra_nest(doc_id):
 def test_unnested_document_of_primitives_and_matching_django_model_has_no_mismatches(
     mock_pymongo_result, mock_django, doc_id
 ):
-    """
-    Test a Django model that matches a document model doesn't find false mismatches.
 
-    A little overloaded as it's technically testing structure code, and multiple primitive matches, but it's
-    offsetting all the unhappy path tests below
-    """
+    # A little overloaded as it's technically testing structure code, and multiple primitive matches, but it's
+    # offsetting all the unhappy path tests below
+
     for raw_key, raw_val in mock_pymongo_result.items():
         assert (
-            _check_data_structure(
-                model_class=MockDepartment, db_attribute_to_field=MOCK_DEPARTMENT_FIELDS, raw_key=raw_key
+            _get_data_structure_errors(
+                model_class=MockDepartment, db_attribute_to_field=MOCK_DEPARTMENT_FIELDS, document_attribute=raw_key
             )
             is None
         )
         field_name = MOCK_DEPARTMENT_FIELDS[raw_key]
         assert (
-            _check_data_values(
+            _get_data_value_errors(
                 model_class=MockDepartment,
                 field_name=field_name,
                 raw_val=raw_val,
@@ -80,26 +78,17 @@ def test_unnested_document_of_primitives_and_matching_django_model_has_no_mismat
         )
 
 
-def test_check_data_structure_detects_missing_attribute():
-    """
-    Test check_data_structure() correctly finds cases where the document has extra attributes compared to the django
-    model.
-
-    This functionality catches missed mappings (or bad data).
-    """
+def test_get_data_structure_errors_detects_missing_attribute():
     extra_attribute = "extra_attribute"
-    mismatch = _check_data_structure(
-        model_class=MockDepartment, db_attribute_to_field=MOCK_DEPARTMENT_FIELDS, raw_key=extra_attribute
+    mismatch = _get_data_structure_errors(
+        model_class=MockDepartment, db_attribute_to_field=MOCK_DEPARTMENT_FIELDS, document_attribute=extra_attribute
     )
     assert extra_attribute in mismatch
     assert "missing" in mismatch
 
 
-def test_check_data_values_detects_value_mismatch(mock_django, doc_id):
-    """
-    Test check_data_values() correctly finds cases where the documents value and the django value don't match.
-    """
-    mismatches = _check_data_values(
+def test_get_data_value_errors_detects_value_mismatch(mock_django, doc_id):
+    mismatches = _get_data_value_errors(
         model_class=MockDepartment,
         field_name="num_of_employees",
         raw_val=NUM_EMPLOYEES + 1,
@@ -111,16 +100,13 @@ def test_check_data_values_detects_value_mismatch(mock_django, doc_id):
     assert "values_changed" in mismatches[0]
 
 
-def test_check_data_values_detects_none_vs_value_mismatch(mock_django, doc_id):
+def test_get_data_value_errors_detects_none_vs_value_mismatch(mock_django, doc_id):
     """
-    Test check_data_values() can detect errors where db is null and django is specifying a non-standard default value
-    instead of "" or None.
-
-    This is a useful feature because it can catch unintended data corruption from loading a null database value and
-    then on save, giving it a default value that looks like real data.
+    Test get_data_value_errors() can detect errors where db is null and django is specifying a non-standard default
+    value instead of "" or None.
     """
     # DB has null, Django hydrated a default string
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartment, field_name="name", raw_val=None, doc_id=doc_id, django_obj=mock_django
     )
 
@@ -129,11 +115,12 @@ def test_check_data_values_detects_none_vs_value_mismatch(mock_django, doc_id):
     assert "name" in mismatches[0]
 
 
-def test_check_data_values_detects_value_type_mismatch(mock_django, doc_id):
+def test_get_data_value_errors_detects_value_type_mismatch(mock_django, doc_id):
     """
-    Test check_data_values() detects different value types even if the values are logically equivalent like 2 and two.
+    Test get_data_value_errors() detects different value types even if the values are logically equivalent like 2 and
+    two.
     """
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartment,
         field_name="num_of_employees",
         raw_val=str(NUM_EMPLOYEES),
@@ -149,26 +136,22 @@ def test_check_data_values_detects_value_type_mismatch(mock_django, doc_id):
 def test_nested_pymongo_result_values_can_match_embedded_models(
     mock_pymongo_result, doc_id, mock_department_ultra_nest
 ):
-    """
-    Test documents with embedded documents that match django models with embedded models cause no mismatches.
-
-    A little overloaded as it's technically testing structure code and value code, but it's offsetting all the unhappy
-    path tests below
-    """
+    # A little overloaded as it's technically testing structure code and value code, but it's offsetting all the unhappy
+    # path tests below
 
     mock_pymongo_result["nestOne"] = {"nestTwo": {"mockNestWithValue": {"actualVal": 2}}}
     for raw_key, raw_val in mock_pymongo_result.items():
         assert (
-            _check_data_structure(
+            _get_data_structure_errors(
                 model_class=MockDepartmentUltraNest,
                 db_attribute_to_field=MOCK_DEPARTMENT_ULTRA_NEST_FIELDS,
-                raw_key=raw_key,
+                document_attribute=raw_key,
             )
             is None
         )
         field_name = MOCK_DEPARTMENT_ULTRA_NEST_FIELDS[raw_key]
         assert (
-            _check_data_values(
+            _get_data_value_errors(
                 model_class=MockDepartmentUltraNest,
                 field_name=field_name,
                 raw_val=raw_val,
@@ -180,16 +163,13 @@ def test_nested_pymongo_result_values_can_match_embedded_models(
 
 
 def test_nested_pymongo_result_value_mismatches_detectable(mock_pymongo_result, doc_id, mock_department_ultra_nest):
-    """
-    Test that mismatches can be found even if the mismatch is deeply embedded.
+    # This does depend on test_check_data_values_detects_value_type_mismatch() passing to be useful data
 
-    This does depend on test_check_data_values_detects_value_type_mismatch() passing to be useful data
-    """
     mock_pymongo_result["nestOne"] = {"nestTwo": {"mockNestWithValue": {"actualVal": "2"}}}
     mismatches = []
     for raw_key, raw_val in mock_pymongo_result.items():
         field_name = MOCK_DEPARTMENT_ULTRA_NEST_FIELDS[raw_key]
-        mismatches += _check_data_values(
+        mismatches += _get_data_value_errors(
             model_class=MockDepartmentUltraNest,
             field_name=field_name,
             raw_val=raw_val,
@@ -202,14 +182,6 @@ def test_nested_pymongo_result_value_mismatches_detectable(mock_pymongo_result, 
 
 
 def test_strip_django_defaults_removes_irrelevant_defaults():
-    """
-    Test that strip_django_defaults() removes irrelevant defaults.
-
-    Documents can omit attributes entirely. Django models can't omit the existence of a property so instead it has a
-    default value for those properties. When comparing data, however, we don't want to falsely flag the difference
-    when the database has literally nothing and Django has "". That's a limitation of mapping nosql in Django that
-    can't be easily solved.
-    """
     pymongo_result_data = {}
     django_data = {"displayTitle": "", "defaultDeclarations": []}
 
@@ -219,11 +191,7 @@ def test_strip_django_defaults_removes_irrelevant_defaults():
     assert django_data == {}
 
 
-def test_strip_django_defaults_doesnt_remove_mismatched_defaults():
-    """
-    Test strip_django_defaults() doesn't remove genuine mismatches when the db has values and the Django model has
-    not been hydrated correctly.
-    """
+def test_strip_django_defaults_doesnt_remove_mismatched_defaults_when_actual_mismatch():
     pymongo_result_data = {"form": {"name": "Street Form", "version": 1}}
     django_data = {"form": {}}
 
@@ -234,24 +202,17 @@ def test_strip_django_defaults_doesnt_remove_mismatched_defaults():
     assert django_data["form"] == {}
 
 
-def test_check_data_values_detects_data_loss_on_embedded_model(doc_id, mock_department_ultra_nest):
-    """
-    Test check_data_values() flags data loss when db contains nested data but the Django embedded model fails to
-    hydrate child fields.
-    """
-    # 1. MongoDB has populated nested data for 'nestOne'
+def test_get_data_value_errors_detects_data_loss_on_embedded_model(doc_id, mock_department_ultra_nest):
     raw_pymongo_result_val = {"nestTwo": {"mockNestWithValue": {"actualVal": 42}}}
 
-    # Django model has nest_one initialized but child data didn't hydrate
-    django_obj = MockDepartmentUltraNest(nest_one=MockNest1())
+    django_obj_with_unhydrated_child = MockDepartmentUltraNest(nest_one=MockNest1())
 
-    # Check values for the 'nest_one' field
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartmentUltraNest,
         field_name="nest_one",
         raw_val=raw_pymongo_result_val,
         doc_id=doc_id,
-        django_obj=django_obj,
+        django_obj=django_obj_with_unhydrated_child,
     )
 
     assert len(mismatches) > 0
@@ -259,10 +220,7 @@ def test_check_data_values_detects_data_loss_on_embedded_model(doc_id, mock_depa
     assert "nest_one" in mismatches[0]
 
 
-def test_check_data_values_ignores_matching_arrays(doc_id):
-    """
-    Test check_data_values ignores when arrays match correctly.
-    """
+def test_get_data_value_errors_ignores_matching_arrays(doc_id):
     array_list_pymongo_result = [
         {"actualVal": "one"},
         {"actualVal": "two"},
@@ -273,7 +231,7 @@ def test_check_data_values_ignores_matching_arrays(doc_id):
     )
 
     field_name = MOCK_DEPARTMENT_WITH_ARRAY_LIST_FIELDS["arrayList"]
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartmentWithArrayList,
         field_name=field_name,
         raw_val=array_list_pymongo_result,
@@ -283,34 +241,29 @@ def test_check_data_values_ignores_matching_arrays(doc_id):
     assert len(mismatches) == 0
 
 
-def test_check_data_values_detects_array_length_mismatch():
-    """
-    Test check_data_values() correctly detects when an array is missing an item.
-    """
-    # 3  items in pymongo result
-    array_list_pymongo_result = [
+def test_get_data_value_errors_detects_array_length_mismatch():
+    three_pymongo_results = [
         {"actualVal": "one"},
         {"actualVal": "two"},
         {"actualVal": "three"},
     ]
 
-    # 2 django items
-    django_obj = MockDepartmentWithArrayList(
+    django_obj_with_two_array_items = MockDepartmentWithArrayList(
         array_list=[MockNestWithValue(actual_val="one"), MockNestWithValue(actual_val="two")]
     )
     field_name = MOCK_DEPARTMENT_WITH_ARRAY_LIST_FIELDS["arrayList"]
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartmentWithArrayList,
         field_name=field_name,
-        raw_val=array_list_pymongo_result,
+        raw_val=three_pymongo_results,
         doc_id="mock_id_array_test",
-        django_obj=django_obj,
+        django_obj=django_obj_with_two_array_items,
     )
     assert len(mismatches) == 1
     assert "iterable_item_removed at array_list[2]" in mismatches[0]
 
 
-def test_check_data_values_detects_item_value_mismatch_inside_array(doc_id):
+def test_get_data_value_errors_detects_item_value_mismatch_inside_array(doc_id):
     """
     Test check_data_values() detects when arrays are the same length but don't contain the same data
     """
@@ -320,7 +273,7 @@ def test_check_data_values_detects_item_value_mismatch_inside_array(doc_id):
     )
 
     field_name = MOCK_DEPARTMENT_WITH_ARRAY_LIST_FIELDS["arrayList"]
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartmentWithArrayList,
         field_name=field_name,
         raw_val=array_list_pymongo_result,
@@ -333,13 +286,7 @@ def test_check_data_values_detects_item_value_mismatch_inside_array(doc_id):
     assert "array_list[1]" in mismatches[0]
 
 
-def test_check_data_values_detects_missing_nested_properties(doc_id, mock_department_ultra_nest):
-    """
-    Test check_data_values() detects missing sub-properties inside nested models.
-
-    Check_data_structure() guards against unmapped top-level document attributes, check_data_values() relies on DeepDiff
-    to recursively inspect embedded models and flag missing keys that are nested lower down.
-    """
+def test_get_data_value_errors_detects_missing_nested_properties(doc_id, mock_department_ultra_nest):
     nested_pymongo_result_val = {
         "nestTwo": {
             "mockNestWithValue": {
@@ -349,7 +296,7 @@ def test_check_data_values_detects_missing_nested_properties(doc_id, mock_depart
         }
     }
 
-    mismatches = _check_data_values(
+    mismatches = _get_data_value_errors(
         model_class=MockDepartmentUltraNest,
         field_name="nest_one",
         raw_val=nested_pymongo_result_val,
