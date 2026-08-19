5807c512859599e5e7fab1c0038227d542e0f7e9
simonfreeman-ux <simon.freeman@digital.cabinet-office.gov.uk>
2026-08-19 12:49:12 +0100

    rename setting model, fix for hydration tests

--- common/models/setting.py
+++ common/models/setting.py
@@ -3,7 +3,7 @@ from django.db import models
 from django_mongodb_backend.fields import ObjectIdField
 
 
-class Settings(models.Model):
+class Setting(models.Model):
     _id = ObjectIdField(default=bson.ObjectId, unique=True, editable=False, primary_key=True)
     key = models.CharField(max_length=255, unique=True)
     value = models.CharField(max_length=255, unique=True)
