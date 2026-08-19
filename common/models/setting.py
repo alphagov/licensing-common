be550d25cf475043ec9bc70318769b730d9dcc4a
simonfreeman-ux <simon.freeman@madetech.com>
2026-08-19 14:30:16 +0100

    [GDSL-151]Feat:add setting model (#25)
    Add a database model for storing settings in the database. (not project settings, settings that users can set)

---
+++ common/models/setting.py
@@ -0,0 +1,20 @@
+import bson
+from django.db import models
+from django_mongodb_backend.fields import ObjectIdField
+
+
+class Setting(models.Model):
+    _id = ObjectIdField(default=bson.ObjectId, unique=True, editable=False, primary_key=True)
+    key = models.CharField(max_length=255, unique=True)
+    value = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        db_table = "settings"
+        managed = False
+
+    def __str__(self):
+        return f"{self.key}"
+
+    @property
+    def id(self):
+        return self.key
--- common/tests/hydration/test_model_hydration_and_data_integrity.py
+++ common/tests/hydration/test_model_hydration_and_data_integrity.py
@@ -6,6 +6,7 @@ from common.models.authority_payment_accounts import AuthorityPaymentAccounts
 from common.models.department import Department
 from common.models.licences import Licence
 from common.models.payments import Payment
+from common.models.setting import Setting
 from common.tests.utils.hydration import verify_model_against_collection
 from config import settings
 
@@ -19,10 +20,11 @@ from config import settings
         Department,
         Licence,
         Payment,
+        Setting,
     ],
 )
 def test_django_models_match_document(show_diffs, model):
     with MongoClient(settings.DOCUMENT_DB_CONN) as client:
         db = client["licensify"]
-        errors = verify_model_against_collection(db, model, "all", show_diffs)
+        errors = verify_model_against_collection(db, model, 0, show_diffs)
         assert errors == []
